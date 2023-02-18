3e3e8b7d3ef4e1efa937bdaf72cd7b38affe1ce0
aashirisrar <aashirisrar968@gmail.com>
2023-02-18 19:10:21 +0500

    added the symmetric function

--- OOPS Course/Assignments/A1/Q1.cpp
+++ OOPS Course/Assignments/A1/Q1.cpp
@@ -78,19 +78,44 @@ int main()
 
     /* Transpose of Matrix */
     // A
-    // int **transposeOfA = TransposeMatrix(matrixOne, rowColSizeArr[0][0], rowColSizeArr[0][1]);
-    // cout << "Transpose of A:" << endl;
-    // OutputMatrix(transposeOfA, rowColSizeArr[0][0], rowColSizeArr[0][1]);
+    int **transposeOfA = TransposeMatrix(matrixOne, rowColSizeArr[0][0], rowColSizeArr[0][1]);
+    cout << "Transpose of A:" << endl;
+    OutputMatrix(transposeOfA, rowColSizeArr[0][0], rowColSizeArr[0][1]);
 
-    // // B
-    // int **transposeOfB = TransposeMatrix(matrixTwo, rowColSizeArr[1][0], rowColSizeArr[1][1]);
-    // cout << "Transpose of B:" << endl;
-    // OutputMatrix(transposeOfB, rowColSizeArr[1][0], rowColSizeArr[1][1]);
+    // B
+    int **transposeOfB = TransposeMatrix(matrixTwo, rowColSizeArr[1][0], rowColSizeArr[1][1]);
+    cout << "Transpose of B:" << endl;
+    OutputMatrix(transposeOfB, rowColSizeArr[1][0], rowColSizeArr[1][1]);
 
     // C
     int **transposeOfC = TransposeMatrix(matrixThree, rowColSizeArr[2][0], rowColSizeArr[2][1]);
     cout << "Transpose of C:" << endl;
     OutputMatrix(transposeOfC, rowColSizeArr[2][1], rowColSizeArr[2][0]);
+
+    /* SYMMETRIC */
+    // A
+    bool isSymmetricA = IsSymmetric(transposeOfA, rowColSizeArr[0][0], rowColSizeArr[0][1]);
+    if (isSymmetricA)
+    {
+        cout << "Matrix A is symmetric" << endl;
+    }
+    else
+    {
+        cout << "Matrix A is NOT symmetric" << endl;
+    }
+
+    // B
+    bool isSymmetricB = IsSymmetric(transposeOfB, rowColSizeArr[1][0], rowColSizeArr[1][1]);
+    if (isSymmetricB)
+    {
+        cout << "Matrix B is symmetric" << endl;
+    }
+    else
+    {
+        cout << "Matrix B is NOT symmetric" << endl;
+    }
+
+    /* Interchanging Rows */
 
     return 0;
 }
@@ -215,5 +240,35 @@ int **TransposeMatrix(int **matrix, const int &ROWS, const int &COLS)
         }
         return transposeMatrix;
     }
+    return 0;
+}
+
+bool IsSymmetric(int **matrix, const int &ROWS, const int &COLS)
+{
+    int **transposeOfTransposeMatrix = new int *[ROWS];
+    for (int i = 0; i < ROWS; i++)
+    {
+        transposeOfTransposeMatrix[i] = new int[COLS];
+    }
+
+    transposeOfTransposeMatrix = TransposeMatrix(matrix, ROWS, COLS);
+
+    int count = 0;
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            if (transposeOfTransposeMatrix[i][j] == matrix[i][j])
+            {
+                count++;
+            }
+        }
+    }
+
+    if (count == ROWS * COLS)
+    {
+        return 1;
+    }
+
     return 0;
 }
