9046529d96b71b3f24a3c60b3ba8c4fe50fb59c7
aashirisrar <aashirisrar968@gmail.com>
2023-03-10 18:08:57 +0500

    completed half

--- OOPS Course/Homework/HW2/Q1.cpp
+++ OOPS Course/Homework/HW2/Q1.cpp
@@ -14,38 +14,89 @@ class FeetInches
     and 5 feet 2 inches would be adjusted to 4 feet and 10 inches.*/
     void simplify()
     {
+        if (inches >= 12)
+        {
+            feet += inches / 12;
+            inches %= 12;
+        }
+        else if (inches < 0)
+        {
+            feet -= abs(inches) / 12 + 1;
+            inches = 12 - abs(inches) % 12;
+        }
     }
 
 public:
     // constructor
     //  write copy constructor if there is need
     // overload assignment operator, if there is need
-    FeetInches()
-    {
-    }
 
+    // default constructor
     FeetInches(int f = 0, int i = 0)
     {
         feet = f;
         inches = i;
         simplify();
-        // mutator function
-        // accessor function
+    }
+
+    // copy constructor
+    FeetInches(const FeetInches &obj)
+    {
+        feet = obj.feet;
+        inches = obj.inches;
+    }
+
+    // overloaded = operator
+    void operator=(const FeetInches &obj)
+    {
+        feet = obj.feet;
+        inches = obj.inches;
+    }
+
+    // +,-,*,/,>,<,>=,<= operators for the same class.
+
+    // mutator function - setters
+    void feetSetter(int f)
+    {
+        feet = f;
+    }
+
+    void inchesSetter(int i)
+    {
+        inches = i;
+    }
+
+    // accessor function - getters
+    int feetGetter()
+    {
+        return feet;
+    }
+
+    int inchesGetter()
+    {
+        return inches;
+    }
+
+    void display()
+    {
+        cout << feet << " " << inches << endl;
     }
 };
 
 int main()
 {
     FeetInches f1;
-    // display f1
+    f1.display();
+
     FeetInches f2 = f1;
-    // display f2
+    f2.display();
+
     FeetInches f3(5, 18);
-    // display f3
+    f3.display();
+
     f1 = f3;
-    // display f1
-    // dsipaly f3
-    system("puase");
+    f1.display();
+    f3.display();
 
     return 0;
 }
